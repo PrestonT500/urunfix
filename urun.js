6beeffdd38d7f33c80e5d26f2add6e85bc9fea75
Preston <PrestonT500WasTaken@proton.me>
2024-02-08 14:10:41 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -4,6 +4,7 @@ document.addEventListener("keydown", function (e) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
 		(e.src = "https://prestont500.github.io/urunfix/popup.html"),
+			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "uRun",
 			t.addEventListener("message", function (e) {
