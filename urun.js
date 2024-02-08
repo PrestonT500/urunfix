7504f8b97861dca67391eeb259ae1c93fe84a801
Preston <PrestonT500WasTaken@proton.me>
2024-02-08 10:48:22 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,14 +1,23 @@
-/// urun.js
-document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//inglan2.github.io/uRun/popup.html"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});
-	}
+document.addEventListener("keydown", function (event) {
+    if (event.key === "~" && event.ctrlKey) {
+        var popupWindow = window.open("", "_blank", "width=500,height=300");
+        var iframe = popupWindow.document.createElement("iframe");
+        iframe.src = "https://prestont500.github.io/urunprivate/popup.html"; // Ensure using HTTPS
+        iframe.style.cssText = "width:100%; height:100%; border:none;";
+        popupWindow.document.body.appendChild(iframe);
+        popupWindow.document.title = "uRun";
+
+        popupWindow.addEventListener("message", function (event) {
+            if (event.data.toString().startsWith("execute:")) {
+                try {
+                    // Execute the received command
+                    eval(event.data.toString().replace("execute:", ""));
+                    // Close the popup window
+                    popupWindow.close();
+                } catch (error) {
+                    console.error("Error executing command:", error);
+                }
+            }
+        });
+    }
 });
