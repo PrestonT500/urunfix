8a328022b174be120e0ee3000b1d91ee92b68fa8
Preston <PrestonT500WasTaken@proton.me>
2024-02-08 13:43:57 -0800

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,14 +1,12 @@
 document.addEventListener("keydown", function (e) {
-    if (e.key === "`" && e.ctrlKey && e.altKey) {
-        var popup = window.open("//prestont500.github.io/urunfix/popup.html", "_blank", "width=500,height=300");
-
+    if (e.key == "~" && e.ctrlKey) {
+        var popup = window.open("", "_blank", "width=500,height=300");
         var iframe = popup.document.createElement("iframe");
-        iframe.src = "//prestont500.github.io/urunfix/popup.html";
+        iframe.src = "https://prestont500.github.io/urunfix/popup.html";
         iframe.style.cssText = "width:100%; height:100%; border:none;";
-
         popup.document.body.appendChild(iframe);
         popup.document.title = "uRun";
-
+        
         popup.addEventListener("message", function (event) {
             if (typeof event.data === "string" && event.data.startsWith("execute:")) {
                 try {
